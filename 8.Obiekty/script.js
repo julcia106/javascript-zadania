3bbc41164cba3cdef465356e13dd30bc0f6ee6c8
Julia <juliaarutkowska@gmail.com>
2020-12-31 13:37:04 +0100

    Zadanie 8

--- 8.Obiekty/script.js
+++ 8.Obiekty/script.js
@@ -1,81 +1,81 @@
-// // Zadanie 1 i 2
+// Zadanie 1 i 2
 
-// const car = {
-//     brand : "Volvo",
-//     colour : "Blue",
-//     price : 100000,
-//     changeColour (newColour){
-//         this.colour = newColour;
-//         console.log("New car colour: " + car.colour);
-//     }
-// };
+const car = {
+    brand : "Volvo",
+    colour : "Blue",
+    price : 100000,
+    changeColour (newColour){
+        this.colour = newColour;
+        console.log("New car colour: " + car.colour);
+    }
+};
 
-// console.log("Marka: " + car.brand);
-// console.log("Kolor: " + car.colour);
-// console.log("Cena: " + car.price)
-// car.changeColour("White");
+console.log("Marka: " + car.brand);
+console.log("Kolor: " + car.colour);
+console.log("Cena: " + car.price)
+car.changeColour("White");
 
-// // Zadanie 3
+// Zadanie 3
 
-// const array = [1,2,3,4,5];
+const array = [1,2,3,4,5];
 
-// let tab = {
-//     sum: 0,
-//     sumTab(t) {
+let tab = {
+    sum: 0,
+    sumTab(t) {
 
-//         let innerSum = 0;
+        let innerSum = 0;
 
-//         for(let i of t){
+        for(let i of t){
 
-//             innerSum += i;
-//         }
+            innerSum += i;
+        }
 
-//         this.sum = innerSum;
+        this.sum = innerSum;
 
-//         console.log(this.sum);
-//     }
-// };
+        console.log(this.sum);
+    }
+};
 
-// console.log(tab.sum);
-// tab.sumTab(array);
-// console.log(tab.sum);
+console.log(tab.sum);
+tab.sumTab(array);
+console.log(tab.sum);
 
-// // Zadanie 4
+// Zadanie 4
 
-// const car = {
-//     Brand: "Volvo",
-//     Age: 5,
-//     Colour: "Blue",
-// };
+const car = {
+    Brand: "Volvo",
+    Age: 5,
+    Colour: "Blue",
+};
 
-// for(let key in car){
-//     console.log(key + ": " + car[key]);
-// }
+for(let key in car){
+    console.log(key + ": " + car[key]);
+}
 
-// // Zadanie 5
+// Zadanie 5
 
-// const car = {
+const car = {
 
-//     Brand: "Volvo",
-//     Age: 5,
-//     Colour: "Blue",
+    Brand: "Volvo",
+    Age: 5,
+    Colour: "Blue",
 
-//     Interior : {
-//         Material: "Leather",
-//         Colour: "Brown",
-//         Price: 12000,
-//     },
-// };
+    Interior : {
+        Material: "Leather",
+        Colour: "Brown",
+        Price: 12000,
+    },
+};
 
-// for(let key in car.Interior){
+for(let key in car.Interior){
 
-//     console.log(key + ": " + car.Interior[key]);
-// }
+    console.log(key + ": " + car.Interior[key]);
+}
 
 // Zadanie 6
 
 const car = {
-    
+
     Brand: "Volvo",
     Age: 5,
     Colour: "Blue",
